17b36fb29d901810f6ecf25892d9e43c36ad9e85
ySpac3 <pedrohenriquef243@gmail.com>
2025-05-13 13:32:59 -0300

    Otimização e melhoria na efetividade do código

--- Main.py
+++ Main.py
@@ -1,31 +1,53 @@
 import pandas as pd
-import os 
+import os
 import datetime as dt
 import zipfile
 from time import sleep
 
-def Backup(Tempo,dataframe,Diretorio):
+
+def Backup(Tempo, dataframe, Diretorio):
+
+    with open("lastbp.txt", "r") as arquivo:
+        date = arquivo.readline().strip()
+        if date == ' ':
+            lastbp = dt.datetime.now().date()
+        else:
+            lastbp = dt.datetime.strptime(date,"%Y-%m-%d")
     originaldr = os.path.dirname(Diretorio)
-    dataframe.to_csv(f"{originaldr}\Backup.csv",index = False)
-    Bp = f"Backup.csv"
+    dataframe.to_csv(f"{originaldr}\Backup.csv", index=False)
+    Bp = f"{originaldr}\Backup.csv"
     Zip = f"{originaldr}\Backup.zip"
-    
-    match Tempo:
-        case 1:
-            with zipfile.ZipFile(Zip, 'w') as zipf:
-                zipf.write(Bp)
-            sleep(dt.timedelta(days=1).total_seconds())
-                
-        case 2:
-            with zipfile.ZipFile(Zip, 'w') as zipf:
-                zipf.write(Bp)  
-            sleep(dt.timedelta(days=30).total_seconds())
-                
-        case 3:
-            with zipfile.ZipFile(Zip, 'w') as zipf:
-                zipf.write(Bp)
-            sleep(dt.timedelta(days=365).total_seconds())
-                
+    while True:
+        match Tempo:
+            case 1:
+                nextbp = lastbp + dt.timedelta(days=1)
+                if dt.datetime.now() >= nextbp:
+                    with zipfile.ZipFile(Zip, 'w') as zipf:
+                        zipf.write(Bp)
+                    with open("lastbp.txt", "w") as arquivo:
+                        arquivo.write(dt.datetime.now().date().strftime("%Y-%m-%d"))
+
+            case 2:
+                nextbp = lastbp + dt.timedelta(days=30)
+                if dt.datetime.now() >= nextbp:
+                    with zipfile.ZipFile(Zip, 'w') as zipf:
+                        zipf.write(Bp)
+                    with open("lastbp.txt", "w") as arquivo:
+                        arquivo.write(dt.datetime.now().date().strftime("%Y-%m-%d"))
+
+            case 3:
+                nextbp = lastbp + dt.timedelta(days=365)
+                if dt.datetime.now() >= nextbp:
+                    with zipfile.ZipFile(Zip, 'w') as zipf:
+                        zipf.write(Bp)
+                    with open("lastbp.txt", "w") as arquivo:
+                        arquivo.write(dt.datetime.now().date().strftime("%Y-%m-%d"))
+            case 0:
+                with zipfile.ZipFile(Zip, 'w') as zipf:
+                    zipf.write(Bp)
+                with open("lastbp.txt", "w") as arquivo:
+                    arquivo.write(dt.datetime.now().date().strftime("%Y-%m-%d"))
+        sleep(3600)
 
 while True:
     Diretorio = input("Digite o diretório do arquivo>>")
@@ -34,6 +56,7 @@ while True:
         break
     except:
         print("Arquivo não encontrado")
-    Tempo = int(input("Em quanto tempo deseja fazer o backup? |1-24h|2-30 dias|3-365 dias >>"))
-
-    Backup(True,Tempo,Arquivo,Diretorio)
+Tempo = int(
+    input("Em quanto tempo deseja fazer o backup? |1-24h|2-30 dias|3-365 dias >>"))
+print(dt.datetime.now().date())
+Backup(Tempo, Arquivo, Diretorio)
